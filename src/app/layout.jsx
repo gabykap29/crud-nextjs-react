1e9b5ea9296ddb4aba4022b4f8e471adc0902c62
Gabriel Acosta <gabriacos@gmail.com>
2024-07-18 12:54:13 -0300

    Se configuró para que sea una PWA

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,6 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" href="/icons/icon-192x192.png" />
+          <meta name="theme-color" content="#000000" />
+      </head>
       <body className={inter.className}>
         <Navbar />
         <main className="container mx-auto px-5 mt-4">
